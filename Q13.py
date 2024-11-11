37c0348d343f673fdd18f991e88f373cfc8dcc33
Priyanka22743 <priyankagulati218@gmail.com>
2024-11-11 11:16:55 +0530

    Update Q13.py

--- Q13.py
+++ Q13.py
@@ -9,4 +9,4 @@ power_value = pow(2,3) #8
 print(f"absolute value : {absolute_value}")
 print(f"rounded value: {rounded_value}")
 print(f"power value : {power_value}")
-print("Program by Diya Bhutani - 0221BCA041")
+print("Program by priyanka gulati- 0221BCA040")
